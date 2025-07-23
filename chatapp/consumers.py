4e8b8aa091e75b9f36d81277943ca5b54e113b03
Henrique Cabrita <hmrc_aim@hotmail.com>
2025-07-23 16:29:59 +0100

    Update consumers.py

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -1,8 +1,6 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from asgiref.sync import sync_to_async
-from django.contrib.auth.models import User
-from .models import ChatRoom, ChatMessage
 import logging
 
 logger = logging.getLogger(__name__)  # For error logging
@@ -19,9 +17,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Fixed group_discard arguments
         await self.channel_layer.group_discard(
-            self.room_group_name,  # Correct first argument
+            self.room_group_name,
             self.channel_name
         )
     
@@ -32,7 +29,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             username = data['username']
             room = data['room']
 
-            # Save message before broadcasting
+            # Save message to DB
             await self.save_message(username, room, message)
 
             await self.channel_layer.group_send(
@@ -53,6 +50,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
     @sync_to_async
     def save_message(self, username, room_slug, message):
         try:
+            # 👇 Move imports here
+            from django.contrib.auth.models import User
+            from .models import ChatRoom, ChatMessage
+
             user = User.objects.get(username=username)
             room = ChatRoom.objects.get(slug=room_slug)
             ChatMessage.objects.create(
